b351cc2a70e21536acb55e4c1507730c541cf389
Loïc <monos-oniromancie@live.fr>
2020-05-06 06:19:31 +0200

    Ajout de configuration de l'émulateur

--- compilation.bat
+++ compilation.bat
@@ -3,20 +3,24 @@ echo =========================================
 echo * Compilation pour la megadrive/genesis *
 echo =========================================
 
-
 Rem ======================================================
 Rem * Configuration des liens vers le dossier du SGDK    *
 Rem * Attention le GDK_WIN utilise des \ et le GDK des / *
 Rem ======================================================
 
-set GDK_WIN=I:\0-megadrive\SGDK
-set GDK=I:/0-megadrive/SGDK
-
+set GDK_WIN=W:\SGDK
+set GDK=W:/SGDK
+set Lgens=..\..\kmod\gens.exe
+set Lprojet=..\projets\prisonnier\out\rom.bin
 
 Rem ===========================
 Rem * Commande de compilation *
 Rem ===========================
 %GDK_WIN%\bin\make -f %GDK_WIN%\makefile.gen
 
+Rem ============================
+Rem * Lancement de l'émulateur *
+Rem ============================
+%Lgens% %Lprojet%
 
 pause
